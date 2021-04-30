b2d6c8d0778e93275538c2c06e72910bd9085110
Ashley Rapone <ashleylynnrapone@gmail.com>
2021-04-29 23:13:37 -0400

    Added showPage function

--- js/script.js
+++ js/script.js
@@ -17,7 +17,35 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+  let start_index = (page * 9) - 9;
+  let end_index = page * 9;
 
+  ul_student_list = document.querySelector('.student-list');
+  ul_student_list.innerHTML = '';
+
+  let html = '';
+  for (let i = 0; i < list.length; i++) {
+    if (i >= start_index && i < end_index) {
+      let student_html = `
+         <li class="student-item cf">
+           <div class="student-details">
+             <img class="avatar" src=${ data[i]['picture']['thumbnail'] } alt="Profile Picture">
+             <h3>${ data[i]['name']['first'] } ${ data[i]['name']['last'] }</h3>
+             <span class="email"> ${ data[i]['email'] } </span>
+           </div>
+           <div class="joined-details">
+             <span class="date">Joined ${ data[i]['registered']['date'] }</span>
+           </div>
+         </li>
+      `
+      html += student_html
+    }
+  }
+  ul_student_list.innerHTML = html; 
+}
+
+showPage(data, 1);
 
 
 /*
