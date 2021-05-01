8820588cbbdaa6d7b7ae51440c15b1d3eab003d4
Ashley Rapone <ashleylynnrapone@gmail.com>
2021-04-30 20:19:50 -0400

    added addPagination Function

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function showPage(list, page) {
   let start_index = (page * 9) - 9;
   let end_index = page * 9;
 
-  ul_student_list = document.querySelector('.student-list');
+  let ul_student_list = document.querySelector('.student-list');
   ul_student_list.innerHTML = '';
 
   let html = '';
@@ -45,14 +45,46 @@ function showPage(list, page) {
   ul_student_list.innerHTML = html; 
 }
 
-showPage(data, 1);
+
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+  let pagination_buttons_needed = Math.ceil(list.length / 9);
+  let ul_link_list = document.querySelector('.link-list');
+  let html = '';
+  for ( let i = 1; i <= pagination_buttons_needed; i++ ) {
+    let button_html =  `<li>
+                      <button type="button"> ${i} </button>
+                    </li>`
+    ul_link_list.insertAdjacentHTML('beforeend', button_html);
+    
+  }
+  let first_pagination_button = ul_link_list.querySelector('li:first-child');
+  first_pagination_button.className = 'active';
+  ul_link_list.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+      let current_link = event.target.parentNode;
+      let page_number = event.target.textContent;
+      if (current_link.className === 'active') {
+        showPage(list, page_number);
+      } else if (current_link.className != 'active') {
+          let active_link = ul_link_list.querySelector('.active');
+          active_link.className = '';
+          current_link.className = 'active';
+          showPage(list, page_number);
+      }
+      } 
+  })  
+  }
+
+
 
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
