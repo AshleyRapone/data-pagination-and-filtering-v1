b2a870a26d8a412398482cf16210e4b9ea8e0319
Ashley Rapone <ashleylynnrapone@gmail.com>
2021-05-03 21:50:58 -0400

    Added extra credit tasks

--- js/script.js
+++ js/script.js
@@ -30,12 +30,12 @@ function showPage(list, page) {
       let student_html = `
          <li class="student-item cf">
            <div class="student-details">
-             <img class="avatar" src=${ data[i]['picture']['thumbnail'] } alt="Profile Picture">
-             <h3>${ data[i]['name']['first'] } ${ data[i]['name']['last'] }</h3>
-             <span class="email"> ${ data[i]['email'] } </span>
+             <img class="avatar" src=${ list[i]['picture']['thumbnail'] } alt="Profile Picture">
+             <h3>${ list[i]['name']['first'] } ${ list[i]['name']['last'] }</h3>
+             <span class="email"> ${ list[i]['email'] } </span>
            </div>
            <div class="joined-details">
-             <span class="date">Joined ${ data[i]['registered']['date'] }</span>
+             <span class="date">Joined ${ list[i]['registered']['date'] }</span>
            </div>
          </li>
       `
@@ -56,6 +56,7 @@ function addPagination(list) {
   let pagination_buttons_needed = Math.ceil(list.length / 9);
   let ul_link_list = document.querySelector('.link-list');
   let html = '';
+  ul_link_list.innerHTML = '';
   for ( let i = 1; i <= pagination_buttons_needed; i++ ) {
     let button_html =  `<li>
                       <button type="button"> ${i} </button>
@@ -64,6 +65,10 @@ function addPagination(list) {
     
   }
   let first_pagination_button = ul_link_list.querySelector('li:first-child');
+  if (!first_pagination_button) {
+    ul_link_list.innerHTML = '<p>No Results Found</p>';
+    return
+  }
   first_pagination_button.className = 'active';
   ul_link_list.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
@@ -82,9 +87,51 @@ function addPagination(list) {
   }
 
 
+function showSearchBar() {
+    let search_bar_html = `
+      <label for="search" class="student-search">
+        <span>Search by name</span>
+        <input id="search" placeholder="Search by name...">
+        <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+      </label>
+    `
+    let header = document.querySelector('.header');
+    header.insertAdjacentHTML('beforeend', search_bar_html);
+  }
 
+//function showNoResults() {
+//  let student_list = document.querySelector('.student-list')
+//  let no_results_html = '<p>No results found</p>';
+//  student_list.innerHTML = no_results_html;
+//  console.log(student_list);
+//}
 
 
 // Call functions
 showPage(data, 1);
+showSearchBar();
 addPagination(data);
+
+
+
+
+
+let search_bar_input = document.getElementById('search');
+search_bar_input.addEventListener('keyup', (event) => {
+  let substring = event.target.value
+  let search_results_list = [];
+  console.log(substring);
+  for (let i = 0; i < data.length; i++) {
+    substring = event.target.value.toLowerCase()
+    console.log(substring);
+    let first_name = data[i]['name']['first'].toLowerCase();
+    let last_name = data[i]['name']['last'].toLowerCase();
+    if ( (first_name.includes(substring) || last_name.includes(substring)) ) {
+      search_results_list.push(data[i]);
+    }
+  }
+  console.log(search_results_list);
+  showPage(search_results_list, 1);
+  addPagination(search_results_list);
+})
+
